37017fa6ddb2ddc5a00947cbbf1129be7f84e436
Ricky Indra Gunawan <rickyindra006@gmail.com>
2022-08-05 21:11:36 +0700

    sidebar component

--- src/components/Header/Breadcrumb.js
+++ src/components/Header/Breadcrumb.js
@@ -2,7 +2,7 @@ import { ChevronRightIcon } from "@heroicons/react/outline";
 
 export default function Breadcrumb() {
   return (
-    <div className="grow flex items-center gap-2 ml-28">
+    <div className="grow flex items-center gap-2 ml-24">
       <button className="hover:underline">Perusahaan</button>
       <ChevronRightIcon className="w-4 h-4" />
       <span>Mitramas Infosys Global</span>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,3 +1,24 @@
+import { AdjustmentsIcon, ArchiveIcon, CashIcon, ClipboardCheckIcon, CubeIcon, DeviceMobileIcon, HomeIcon, OfficeBuildingIcon, SearchCircleIcon, SortAscendingIcon, UsersIcon } from "@heroicons/react/outline";
+
 export default function Sidebar() {
-  return <div>Sidebar</div>;
+  return (
+    <div className="fixed left-0 right-0 h-full w-12 flex flex-col items-center gap-6 bg-[#f7f7f7]">
+      <div className="mt-8 w-8 h-8 rounded-full bg-green-400 text-xs text-white font-semibold flex items-center justify-center">M</div>
+      <div className="flex flex-col">
+        <HomeIcon className="w-12 h-10 p-3 text-[#4b4b4b]" />
+        <AdjustmentsIcon className="w-12 h-10 p-3 text-[#4b4b4b]" />
+        <ClipboardCheckIcon className="w-12 h-10 p-3 text-[#4b4b4b]" />
+        <ArchiveIcon className="w-12 h-10 p-3 text-[#4b4b4b]" />
+        <OfficeBuildingIcon className="w-12 h-10 p-3 border-l-4 border-[#41aa58] text-[#4b4b4b] bg-[#e2ebe4]" />
+      </div>
+      <div className="flex flex-col">
+        <UsersIcon className="w-12 h-10 p-3 text-[#4b4b4b]" />
+        <SortAscendingIcon className="w-12 h-10 p-3 text-[#4b4b4b]" />
+        <CubeIcon className="w-12 h-10 p-3 text-[#4b4b4b]" />
+        <CashIcon className="w-12 h-10 p-3 text-[#4b4b4b]" />
+        <SearchCircleIcon className="w-12 h-10 p-3 text-[#4b4b4b]" />
+      </div>
+      <DeviceMobileIcon className="w-12 h-10 p-3 text-[#4b4b4b]" />
+    </div>
+  );
 }
